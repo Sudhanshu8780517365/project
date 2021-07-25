073c74d6bbd5838217e79ac0c24cbaada0c40c5e
Sudhanshu8780517365 <84140944+Sudhanshu8780517365@users.noreply.github.com>
2021-07-25 20:22:34 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -1,7 +1,7 @@
 class Box extends BaseClass {
   constructor(x, y, width, height){
     super(x,y,width,height);
-    this.image = loadImage("sprites/hex.png");
+    this.image = loadImage("hex.png");
   }
   display(){
   if(this.body.speed<7.5){
